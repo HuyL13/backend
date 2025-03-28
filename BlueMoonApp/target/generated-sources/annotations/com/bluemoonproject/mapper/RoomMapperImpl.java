f08497955da8eb3acdac061dd09533096a485e0f
HungTang-120105 <hung.ttm230037@sis.hust.edu.vn>
2025-03-28 20:41:18 +0700

    update dependency

---
+++ BlueMoonApp/target/generated-sources/annotations/com/bluemoonproject/mapper/RoomMapperImpl.java
@@ -0,0 +1,46 @@
+package com.bluemoonproject.mapper;
+
+import com.bluemoonproject.dto.request.RoomRequest;
+import com.bluemoonproject.dto.response.RoomResponse;
+import com.bluemoonproject.entity.Room;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-03-28T20:39:16+0700",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+@Component
+public class RoomMapperImpl implements RoomMapper {
+
+    @Override
+    public Room toRoom(RoomRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        Room.RoomBuilder room = Room.builder();
+
+        room.roomNumber( request.getRoomNumber() );
+        room.floor( request.getFloor() );
+        room.peopleCount( request.getPeopleCount() );
+
+        return room.build();
+    }
+
+    @Override
+    public RoomResponse toRoomResponse(Room room) {
+        if ( room == null ) {
+            return null;
+        }
+
+        RoomResponse.RoomResponseBuilder roomResponse = RoomResponse.builder();
+
+        roomResponse.roomNumber( room.getRoomNumber() );
+        roomResponse.floor( room.getFloor() );
+        roomResponse.peopleCount( room.getPeopleCount() );
+
+        return roomResponse.build();
+    }
+}
---
+++ BlueMoonApp/target/generated-sources/annotations/com/bluemoonproject/mapper/UserMapperImpl.java
@@ -0,0 +1,142 @@
+package com.bluemoonproject.mapper;
+
+import com.bluemoonproject.dto.request.UserActivateRequest;
+import com.bluemoonproject.dto.request.UserCreationRequest;
+import com.bluemoonproject.dto.request.UserUpdateRequest;
+import com.bluemoonproject.dto.response.PermissionResponse;
+import com.bluemoonproject.dto.response.RoleResponse;
+import com.bluemoonproject.dto.response.UserResponse;
+import com.bluemoonproject.entity.Permission;
+import com.bluemoonproject.entity.Role;
+import com.bluemoonproject.entity.User;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-03-28T20:39:16+0700",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User toUser(UserCreationRequest request) {
+        if ( request == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.username( request.getUsername() );
+        user.password( request.getPassword() );
+        user.firstName( request.getFirstName() );
+        user.lastName( request.getLastName() );
+        user.dob( request.getDob() );
+        user.email( request.getEmail() );
+
+        return user.build();
+    }
+
+    @Override
+    public UserResponse toUserResponse(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserResponse.UserResponseBuilder userResponse = UserResponse.builder();
+
+        if ( user.getId() != null ) {
+            userResponse.id( String.valueOf( user.getId() ) );
+        }
+        userResponse.username( user.getUsername() );
+        userResponse.password( user.getPassword() );
+        userResponse.firstName( user.getFirstName() );
+        userResponse.lastName( user.getLastName() );
+        userResponse.email( user.getEmail() );
+        userResponse.dob( user.getDob() );
+        userResponse.roles( roleSetToRoleResponseSet( user.getRoles() ) );
+
+        return userResponse.build();
+    }
+
+    @Override
+    public void updateUser(User user, UserUpdateRequest request) {
+        if ( request == null ) {
+            return;
+        }
+
+        user.setPassword( request.getPassword() );
+        user.setFirstName( request.getFirstName() );
+        user.setLastName( request.getLastName() );
+        user.setDob( request.getDob() );
+        user.setEmail( request.getEmail() );
+    }
+
+    @Override
+    public void activateUser(User user, UserActivateRequest request) {
+        if ( request == null ) {
+            return;
+        }
+
+        user.setPassword( request.getPassword() );
+        user.setFirstName( request.getFirstName() );
+        user.setLastName( request.getLastName() );
+        user.setDob( request.getDob() );
+    }
+
+    protected PermissionResponse permissionToPermissionResponse(Permission permission) {
+        if ( permission == null ) {
+            return null;
+        }
+
+        PermissionResponse.PermissionResponseBuilder permissionResponse = PermissionResponse.builder();
+
+        permissionResponse.name( permission.getName() );
+        permissionResponse.description( permission.getDescription() );
+
+        return permissionResponse.build();
+    }
+
+    protected Set<PermissionResponse> permissionSetToPermissionResponseSet(Set<Permission> set) {
+        if ( set == null ) {
+            return null;
+        }
+
+        Set<PermissionResponse> set1 = new LinkedHashSet<PermissionResponse>( Math.max( (int) ( set.size() / .75f ) + 1, 16 ) );
+        for ( Permission permission : set ) {
+            set1.add( permissionToPermissionResponse( permission ) );
+        }
+
+        return set1;
+    }
+
+    protected RoleResponse roleToRoleResponse(Role role) {
+        if ( role == null ) {
+            return null;
+        }
+
+        RoleResponse.RoleResponseBuilder roleResponse = RoleResponse.builder();
+
+        roleResponse.name( role.getName() );
+        roleResponse.description( role.getDescription() );
+        roleResponse.permissions( permissionSetToPermissionResponseSet( role.getPermissions() ) );
+
+        return roleResponse.build();
+    }
+
+    protected Set<RoleResponse> roleSetToRoleResponseSet(Set<Role> set) {
+        if ( set == null ) {
+            return null;
+        }
+
+        Set<RoleResponse> set1 = new LinkedHashSet<RoleResponse>( Math.max( (int) ( set.size() / .75f ) + 1, 16 ) );
+        for ( Role role : set ) {
+            set1.add( roleToRoleResponse( role ) );
+        }
+
+        return set1;
+    }
+}
