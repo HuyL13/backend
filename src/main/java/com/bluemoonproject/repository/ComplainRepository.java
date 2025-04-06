c0ba926c15838afede4af08400b0bbf9428d0f73
HuyL13 <146957260+HuyL13@users.noreply.github.com>
2025-04-06 10:26:07 +0700

    Update ComplainRepository.java

--- src/main/java/com/bluemoonproject/repository/ComplainRepository.java
+++ src/main/java/com/bluemoonproject/repository/ComplainRepository.java
@@ -1,4 +1,9 @@
 package com.bluemoonproject.repository;
 
-public class ComplainRepository {
+import com.bluemoonproject.entity.Announcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 }
