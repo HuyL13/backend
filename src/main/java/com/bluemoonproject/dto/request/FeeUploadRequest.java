d38ad565d27976a565e44824ba9c9866f1449962
HuyL13 <146957260+HuyL13@users.noreply.github.com>
2025-04-06 10:29:27 +0700

    Update FeeUploadRequest.java

--- src/main/java/com/bluemoonproject/dto/request/FeeUploadRequest.java
+++ src/main/java/com/bluemoonproject/dto/request/FeeUploadRequest.java
@@ -1,4 +1,22 @@
 package com.bluemoonproject.dto.request;
 
+import com.bluemoonproject.enums.FeeStatus;
+import com.bluemoonproject.enums.FeeType;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class FeeUploadRequest {
+    private String description;
+    private LocalDate dueDate;
+
+    private FeeStatus status;
+    private FeeType feeType; // <== Thêm loại phí
 }
