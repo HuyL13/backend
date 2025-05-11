b2dfdb63eb357f075b703888c3c6c79a07960fd3
HuyL13 <146957260+HuyL13@users.noreply.github.com>
2025-05-11 18:19:27 +0700

    lmao

--- src/main/java/com/bluemoonproject/configuration/SecurityConfig.java
+++ src/main/java/com/bluemoonproject/configuration/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
         httpSecurity
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.addAllowedOrigin("https://huyl13.github.io/frontend"); // Allow frontend
+                    config.addAllowedOrigin("https://huyl13.github.io/frontend");
+                    config.addAllowedOrigin("http://localhost:3000");// Allow frontend
                     config.addAllowedMethod("*"); // Allow all methods (GET, POST, OPTIONS, etc.)
                     config.addAllowedHeader("*"); // Allow all headers
                     config.setAllowCredentials(true); // Required if frontend sends cookies or auth headers
